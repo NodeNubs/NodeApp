74c7012180d3dd229b5e1e645c62d6eb6720db37
Ivcho <ivo_rankov@mail.bg>
2016-01-11 19:28:04 +0200

    small change

--- NoRum/data/users.js
+++ NoRum/data/users.js
@@ -8,11 +8,6 @@ module.exports = {
   update: function (name, callback) {
     User.findOne({ username: name }, callback);
   },
-  getByNames: function (redPlayer, blackPlayer, callback) {
-    User.find({
-      $and: [{ $or: [{ username: redPlayer }, { username: blackPlayer }] }]
-    }, callback);
-  },
   getAll: function (callback) {
     User.find(callback);
   }
