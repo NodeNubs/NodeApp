a35c8915c15a30e06328af5ffc7c3be053f2b33b
Ivcho <ivo_rankov@mail.bg>
2016-01-14 12:59:03 +0200

    Removed unused stuff

--- NoRum/app/controllers/ProfileController.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var PATH_PREFIX = 'profile';
-
-module.exports = {
-  getProfileInfo: function (req, res, next) {
-    res.render(PATH_PREFIX + '/profile-info', {loggedUser: req.user})
-  }
-};
--- NoRum/config/express.js
+++ NoRum/config/express.js
@@ -34,11 +34,11 @@ module.exports = function (app, config) {
   app.use(passport.initialize());
   app.use(passport.session());
 
-  /*  app.use(function (req, res, next) {
+   app.use(function (req, res, next) {
    var err = new Error('Not Found');
    err.status = 404;
    next(err);
-   }); shows the user a "friendly msg on serv err" for now keep it comented (easier debugging...)*/
+   });
   app.use(function (req, res, next) {
         if (req.session.error || req.session.success) {
             app.locals.successMessage = req.session.success;
